48eae8fb69dc7ff337e2086c29a3189737334e28
gusrms1210 <gusrms1210@naver.com>
2017-06-27 12:30:25 +0900

    GitHub for git pull

--- git_test/src/main/java/com/test/git/HomeController.java
+++ git_test/src/main/java/com/test/git/HomeController.java
@@ -38,6 +38,8 @@ public class HomeController {
 		System.out.println("Hello Dance World");
 		System.out.println("Add java class file push");
 		System.out.println("World with java");
+		System.out.println("World for pull");
+		
 		return "home";
 	}
 	
